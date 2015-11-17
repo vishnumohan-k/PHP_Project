4743b20efb274f97cb17f58eb0cb01b6bb1c98e4
vishnumohan-k <vishnumohan15@gmail.com>
2015-11-17 20:39:07 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$title="Home";
+$content='<img src="image/coff-cup.jpg" class="imgleft"/>
+<h3>title1</h3>
+<p>
+    Web scraping (web harvesting or web data extraction) is a computer software 
+    technique of extracting information from websites. Usually, such software programs 
+    simulate human exploration of the World Wide Web by either implementing low-level
+    Hypertext Transfer Protocol (HTTP), or embedding a fully-fledged web browser, 
+    such as Mozilla Firefox.
+</p>
+<img src="image/coff2.jpg" class="imgright"/>
+<h3>title2</h3>
+<p>
+    Web scraping is closely related to web indexing, which indexes information on the 
+    web using a bot or web crawler and is a universal technique adopted by most search 
+    engines. In contrast, web scraping focuses more on the transformation of unstructured
+    data on the web, typically in HTML format, into structured data that can be stored and
+    analyzed in a central local database or spreadsheet. Web scraping is also related to 
+    web automation, which simulates human browsing using computer software. Uses of web 
+    scraping include online price comparison, contact scraping, weather data monitoring, 
+    website change detection, research, web mashup and web data integration.
+</p>
+<img src="image/coffie.jpg" class="imgleft"/>
+<h3>title3</h3>
+<p>
+    Web scraping is the process of automatically collecting information from the World
+    Wide Web. It is a field with active developments sharing a common goal with the 
+    semantic web vision, an ambitious initiative that still requires breakthroughs in
+    text processing, semantic understanding, artificial intelligence and human-computer
+    interactions. Current web scraping solutions range from the ad-hoc, requiring human
+    effort, to fully automated systems that are able to convert entire web sites into
+    structured information, with limitations.
+</p>';
+include 'template.php';
+?>
